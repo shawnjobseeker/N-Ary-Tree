48081983ba75341f31996ad1dcd3771f0d912d0d
shawnjobseeker <shawnno1@hotmail.co.uk>
2017-05-01 11:23:08 +0100

    Add project files.

---
+++ ConsoleApp2/Node.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    
+    public class Node<T>
+    {
+        public T data { get; private set; }
+        public Node<T> parent { get; private set; }
+        public List<Node<T>> children { get; private set; }
+        public Node(T data)
+        {
+            this.data = data;
+            this.children = new List<Node<T>>();
+        }
+        public override string ToString()
+        {
+            return data.ToString();
+        }
+        public void AddChild(Node<T> data)
+        {
+            data.parent = this;
+            this.children.Add(data);
+        }
+        public void AddAllChildren(List<Node<T>> children)
+        {
+            foreach (Node<T> child in children)
+                child.parent = this;
+            this.children.AddRange(children);
+        }
+        public bool IsLeaf()
+        {
+            return this.children == null || this.children.Count == 0;
+        }
+        public List<T> PreOrder()
+        {
+            List<T> list = new List<T>();
+            list.Add(data);
+            foreach (Node<T> child in children)
+                list.AddRange(child.PreOrder());
+            return list;
+        }
+        public List<T> PostOrder()
+        {
+            List<T> list = new List<T>();
+            foreach (Node<T> child in children)
+                list.AddRange(child.PreOrder());
+            list.Add(data);
+            return list;
+        }
+        public List<T> LevelOrder()
+        {
+            List<T> list = new List<T>();
+            Queue<Node<T>> queue = new Queue<Node<T>>();
+            queue.Enqueue(this);
+            while(queue.Count != 0)
+            {
+                Node<T> temp = queue.Dequeue();
+                foreach (Node<T> child in temp.children)
+                    queue.Enqueue(child);
+                list.Add(temp.data);
+            }
+            return list;
+        }
+        public int Depth()
+        {
+                int deepest = 0;
+                foreach(Node<T> child in children)
+                {
+                    int depth = 1 + child.Depth();
+                    if (deepest < depth)
+                        deepest = depth;
+                }
+                return deepest;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static int NodeCount = 1;
+        static void Main(string[] args)
+        {
+            
+            Node<Int32> root = new Node<int>(1);
+            root = Generate(root);
+            List<Int32> order  = root.LevelOrder();
+            foreach (int num in order)
+                Console.Write(num + " ");
+            Console.ReadKey();
+        }
+        static Node<int> Generate(Node<int> root)
+        {
+            Random rand = new Random();
+            root.AddAllChildren(Make(rand.Next(0, 5)));
+            for (int i = 0; i < root.children.Count; i++)
+                root.children[i] = Generate(root.children[i]);
+            return root;
+        }
+        static List<Node<int>> Make(int children) {
+            List<Node<int>> list = new List<Node<int>>();
+            if (NodeCount > 100)
+                return list;
+            for (int i = 1; i <= children; i++)
+            {
+                NodeCount++;
+                list.Add(new Node<int>(NodeCount));
+            }
+            return list;
+        }
+    }
+}
